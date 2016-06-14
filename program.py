78810b10bd306a59f0687f010105249897c77802
Marcelus Easley <marcellus.easley@gmail.com>
2016-06-14 02:22:32 -0400

    added logout test

--- program.py
+++ program.py
@@ -12,13 +12,18 @@ from imc.IBMarketingCloud import ApiResult
 if __name__ == "__main__":
     inst = IBMCloud()
 
-    apiresult = inst.authenticate(IBMCloud.AUTH_LEGACY, r"c:\code\ibmapi_config_test.ini")
+    apiresult = inst.login(IBMCloud.AUTH_LEGACY, r"c:\code\ibmapi_config_test.ini")
 
     print(apiresult.status)
     print(apiresult.message)
 
 
-    apir = inst.addRecipient(739357, 0, columns={'Email': 'tester{}@gmail2.com'.format(89)}, updateiffound=True)
+    apir = inst.addRecipient(739357, 0, columns={'Email': 'tester{}@gmail2.com'.format(8329)}, updateiffound=True)
 
     print(apir.status)
     print(apir.message)
+
+    apir2 = inst.logout()
+
+    print(apir2.status)
+    print(apir2.message)
